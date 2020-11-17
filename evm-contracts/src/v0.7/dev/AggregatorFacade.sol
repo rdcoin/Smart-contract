cc96592cf7563d8eb50d0f8e07d446548120729c
Alex Roan <alex.roan@hotmail.com>
2020-11-17 14:31:09 +0000

    Remove underscores from AggregatorFacade

--- evm-contracts/src/v0.7/dev/AggregatorFacade.sol
+++ evm-contracts/src/v0.7/dev/AggregatorFacade.sol
@@ -19,13 +19,13 @@ contract AggregatorFacade is AggregatorV2V3Interface {
   string constant private V3_NO_DATA_ERROR = "No data present";
 
   constructor(
-    address _aggregator,
-    uint8 _decimals,
-    string memory _description
+    address aggregatorAddress,
+    uint8 decimalPlaces,
+    string memory feedDescription
   ) public {
-    aggregator = AggregatorInterface(_aggregator);
-    decimals = _decimals;
-    description = _description;
+    aggregator = AggregatorInterface(aggregatorAddress);
+    decimals = decimalPlaces;
+    description = feedDescription;
   }
 
   /**
@@ -84,13 +84,13 @@ contract AggregatorFacade is AggregatorV2V3Interface {
   /**
    * @notice get data about the latest round. Consumers are encouraged to check
    * that they're receiving fresh data by inspecting the updatedAt value.
-   * @return roundId is the round ID for which data was retrieved
+   * @return id is the round ID for which data was retrieved
    * @return answer is the answer for the given round
    * @return startedAt is always equal to updatedAt because the underlying
    * Aggregator contract does not expose this information.
    * @return updatedAt is the timestamp when the round last was updated (i.e.
    * answer was last computed)
-   * @return answeredInRound is always equal to roundId because the underlying
+   * @return answeredInRound is always equal to id because the underlying
    * Aggregator contract does not expose this information.
    * @dev Note that for rounds that haven't yet received responses from all
    * oracles, answer and updatedAt may change between queries.
@@ -101,83 +101,83 @@ contract AggregatorFacade is AggregatorV2V3Interface {
     virtual
     override
     returns (
-      uint80 roundId,
+      uint80 id,
       int256 answer,
       uint256 startedAt,
       uint256 updatedAt,
       uint80 answeredInRound
     )
   {
-    return _getRoundData(uint80(aggregator.latestRound()));
+    return getRoundDetails(uint80(aggregator.latestRound()));
   }
 
   /**
    * @notice get past rounds answers
-   * @param _roundId the answer number to retrieve the answer for
+   * @param roundId the answer number to retrieve the answer for
    *
    * @dev #[deprecated] Use getRoundData instead. This does not error if no
    * answer has been reached, it will simply return 0. Either wait to point to
    * an already answered Aggregator or use the recommended getRoundData
    * instead which includes better verification information.
    */
-  function getAnswer(uint256 _roundId)
+  function getAnswer(uint256 roundId)
     external
     view
     virtual
     override
     returns (int256)
   {
-    return aggregator.getAnswer(_roundId);
+    return aggregator.getAnswer(roundId);
   }
 
   /**
    * @notice get block timestamp when an answer was last updated
-   * @param _roundId the answer number to retrieve the updated timestamp for
+   * @param roundId the answer number to retrieve the updated timestamp for
    *
    * @dev #[deprecated] Use getRoundData instead. This does not error if no
    * answer has been reached, it will simply return 0. Either wait to point to
    * an already answered Aggregator or use the recommended getRoundData
    * instead which includes better verification information.
    */
-  function getTimestamp(uint256 _roundId)
+  function getTimestamp(uint256 roundId)
     external
     view
     virtual
     override
     returns (uint256)
   {
-    return aggregator.getTimestamp(_roundId);
+    return aggregator.getTimestamp(roundId);
   }
 
   /**
    * @notice get data about a round. Consumers are encouraged to check
    * that they're receiving fresh data by inspecting the updatedAt value.
-   * @param _roundId the round ID to retrieve the round data for
-   * @return roundId is the round ID for which data was retrieved
+   * @param roundId the round ID to retrieve the round data for
+   * @return id is the round ID for which data was retrieved
    * @return answer is the answer for the given round
    * @return startedAt is always equal to updatedAt because the underlying
    * Aggregator contract does not expose this information.
    * @return updatedAt is the timestamp when the round last was updated (i.e.
    * answer was last computed)
-   * @return answeredInRound is always equal to roundId because the underlying
+   * @return answeredInRound is always equal to id because the underlying
    * Aggregator contract does not expose this information.
    * @dev Note that for rounds that haven't yet received responses from all
    * oracles, answer and updatedAt may change between queries.
    */
-  function getRoundData(uint80 _roundId)
+  function getRoundData(uint80 roundId)
     external
     view
     virtual
     override
     returns (
-      uint80 roundId,
+      uint80 id,
       int256 answer,
       uint256 startedAt,
       uint256 updatedAt,
       uint80 answeredInRound
     )
   {
-    return _getRoundData(_roundId);
+    return getRoundDetails(roundId);
   }
 
 
@@ -185,23 +185,23 @@ contract AggregatorFacade is AggregatorV2V3Interface {
    * Internal
    */
 
-  function _getRoundData(uint80 _roundId)
+  function getRoundDetails(uint80 roundId)
     internal
     view
     returns (
-      uint80 roundId,
+      uint80 id,
       int256 answer,
       uint256 startedAt,
       uint256 updatedAt,
       uint80 answeredInRound
     )
   {
-    answer = aggregator.getAnswer(_roundId);
-    updatedAt = uint64(aggregator.getTimestamp(_roundId));
+    answer = aggregator.getAnswer(roundId);
+    updatedAt = uint64(aggregator.getTimestamp(roundId));
 
     require(updatedAt > 0, V3_NO_DATA_ERROR);
 
-    return (_roundId, answer, updatedAt, updatedAt, _roundId);
+    return (roundId, answer, updatedAt, updatedAt, roundId);
   }
 
 }
